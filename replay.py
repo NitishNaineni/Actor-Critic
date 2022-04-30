0b6171b6ce39ce4a685c352173a69d3cb415978d
Nitish-Naineni <96668765+Nitish-Naineni@users.noreply.github.com>
2022-04-30 18:24:27 -0400

    added replay

---
+++ replay.py
@@ -0,0 +1,17 @@
+from collections import namedtuple,deque
+
+translate = namedtuple('translate',
+                        ('state', 'action', 'reward', 'next_state', 'done'))
+class ReplayMemory():
+
+    def __init__(self, capacity):
+        self.memory = deque([],maxlen=capacity)
+
+    def push(self, *args):
+        self.memory.append(translate(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def size(self):
+        return len(self.memory)
